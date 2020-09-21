6dc5a8a5bbe6a1d5f09fc87b76744fa91dc89983
Nathan Jaber <nathanjaber2014@gmail.com>
2020-09-21 07:17:11 -0300

    First commit

---
+++ src/main/java/com/nathan/swplanets/SwplanetsApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.swplanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwplanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwplanetsApplication.class, args);
+	}
+
+}
