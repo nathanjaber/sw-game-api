04ddde5335912b9b000552735324aecda526add3
Nathan Jaber <nathanjaber2014@gmail.com>
2020-09-22 08:14:22 -0300

    Operation POST /planets adjusted to create a planet in the db

--- src/main/java/com/nathan/swplanets/adapter/inbound/HttpPlanetInboundAdapter.java
+++ src/main/java/com/nathan/swplanets/adapter/inbound/HttpPlanetInboundAdapter.java
@@ -1,9 +1,14 @@
 package com.nathan.swplanets.adapter.inbound;
 
+import com.nathan.swplanets.domain.Planet;
+import com.nathan.swplanets.domain.PlanetDTO;
 import com.nathan.swplanets.service.PlanetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/swplanets/v1/planets")
@@ -18,8 +23,15 @@ public class HttpPlanetInboundAdapter {
     }
 
     @PostMapping()
-    public String createPlanet() {
-        return "Planeta criado";
+    public ResponseEntity createPlanet(@RequestBody Planet planet) {
+        try {
+            PlanetDTO newPlanet = service.createPlanet(planet);
+
+            URI location = getUri(newPlanet.get_id());
+            return ResponseEntity.created(location).build();
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/{id}")
@@ -30,5 +42,9 @@ public class HttpPlanetInboundAdapter {
     @DeleteMapping("/{id}")
     public String deletePlanetById(@PathVariable("id") Long id) {
         return "Planeta " + id +" removido";
+    }
+
+    private URI getUri(String id) {
+        return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
     }
 }
--- src/main/java/com/nathan/swplanets/service/PlanetService.java
+++ src/main/java/com/nathan/swplanets/service/PlanetService.java
@@ -5,6 +5,7 @@ import com.nathan.swplanets.domain.PlanetDTO;
 import com.nathan.swplanets.repository.PlanetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,5 +19,11 @@ public class PlanetService {
 
     public List<PlanetDTO> getPlanets() {
         return repository.findAll().stream().map(PlanetDTO::create).collect(Collectors.toList());
+    }
+
+    public PlanetDTO createPlanet(Planet planet) {
+        Assert.isNull(planet.get_id(), "id not informed");
+
+        return PlanetDTO.create(repository.save(planet));
     }
 }
