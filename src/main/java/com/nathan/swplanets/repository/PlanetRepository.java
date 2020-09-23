c2ddb8ec306a881a37d939980416608e8608c3be
Nathan Jaber <nathanjaber2014@gmail.com>
2020-09-23 03:49:07 -0300

    PlanetRepository modified to return List<PlanetDTO>

--- src/main/java/com/nathan/swplanets/repository/PlanetRepository.java
+++ src/main/java/com/nathan/swplanets/repository/PlanetRepository.java
@@ -1,13 +1,13 @@
 package com.nathan.swplanets.repository;
 
 import com.nathan.swplanets.domain.Planet;
+import com.nathan.swplanets.domain.PlanetDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface PlanetRepository extends MongoRepository<Planet, String> {
-    List<Planet> findAll();
+    List<PlanetDTO> findByName(String name);
 }
